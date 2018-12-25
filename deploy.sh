b18979be35f9a16570fe5006bfcc84c79c2ee427
Christophe Benz <christophe.benz@gmail.com>
2018-12-26 00:41:03 +0100

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+npm run build
+git branch -D heroku
+git checkout --orphan heroku
+git add -A
+# git add --force __sapper__/build static/server.css
+git commit -m update
+git push --force heroku heroku:master
+git checkout master
